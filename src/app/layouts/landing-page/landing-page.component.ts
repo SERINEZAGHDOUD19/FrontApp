37789b63756829483e5c7906a81ac0e17e827269
ahmedghribi <ahmedghribi51@gmail.com>
2022-05-20 15:16:50 +0100

    final

--- src/app/layouts/landing-page/landing-page.component.ts
+++ src/app/layouts/landing-page/landing-page.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,5 +6,8 @@ import { Component } from '@angular/core';
   templateUrl: './landing-page.component.html',
 })
 export class LandingPageComponent {
-  constructor() {}
+  constructor(private router: Router) {}
+  goToLogin() {
+    this.router.navigateByUrl('/login');
+  }
 }
